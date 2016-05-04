5ef7ba4a4bfc3857c1b27541e34b1be349a9e2f6
Elena Willen <lnamaso@hotmail.com>
2016-05-04 17:09:56 +0100

    Initial Commit

---
+++ JsonApi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JsonApi
+//
+//  Created by Elena Maso Willen on 04/05/2016.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JsonApi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JsonApi
+//
+//  Created by Elena Maso Willen on 04/05/2016.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
